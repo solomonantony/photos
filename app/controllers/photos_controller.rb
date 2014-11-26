e923b27195f5909f0b35d23f7c1ade950f8cb963
Solomon Antony <santony@murraystate.edu>
2014-11-26 06:58:12 -0600

    Adding Photos functionality

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,16 @@
+class PhotosController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def create
+  end
+end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,5 @@
 class Album < ActiveRecord::Base
   has_many :photos, dependent: :destroy
+  validates :name, presence: true
+   
 end
